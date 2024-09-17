a8d65ba7f6daacc666b1d35c9d76088fb9ba759c
Cesar Peres <cperes84@gmail.com>
2024-09-17 18:41:07 -0300

    chore: ajustar novo formato de geração de código

--- src/services/codeServices.js
+++ src/services/codeServices.js
@@ -1,4 +1,19 @@
-import axios from 'axios';
+// import axios from 'axios';
 
-export const getCode =  () => // responseLoadData;
-   axios.get(`${process.env.NEXT_PUBLIC_API_URL}/codly`);
+/* export const getCode =  () => // responseLoadData;
+   axios.get(`${process.env.NEXT_PUBLIC_API_URL}/codly`); */
+
+import formatDate from "../utility/formatDate";
+export const getCode = async () => {
+  await new Promise((r) => setTimeout(r, 200));
+  const today = formatDate({ date: Date.now(), formatString: "yyyy-MM-dd" });
+  const digit1 = parseInt(today.split("-")[2][1], 10);
+  const digit2 = parseInt(today.split("-")[1][1], 10);
+  const digit3 = parseInt(today.split("-")[2][0], 10);
+  const code = `${digit1}${digit2}${digit3}`;
+
+  return {
+    data:
+      { code, date: today },
+  };
+};
